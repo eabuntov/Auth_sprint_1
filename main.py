02a5fd677bde46b37d94e7916f5b22159ae7647d
eabuntov <e.a.buntov@gmail.com>
2025-11-23 09:04:31 +0500

    openapi.json extension

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import json
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from api.v1.api_router import api_router
 
 app = FastAPI()
+
+with open("api/v1/openapi.json", "r", encoding="utf-8") as f:
+    custom_openapi_schema = json.load(f)
+
+def custom_openapi():
+    return custom_openapi_schema
+
+app.openapi = custom_openapi
 
 app.include_router(api_router)
 
